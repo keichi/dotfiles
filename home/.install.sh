8acbef91d87044299446035c32d851ee52ee3552
Keichi Takahashi <keichi.t@me.com>
2015-02-23 23:49:11 +0900

    Start using brewdler

--- home/.install.sh
+++ home/.install.sh
@@ -2,9 +2,10 @@
 
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap Homebrew/brewdler
 
-# Install Homebrew Casks
-brew bundle
+# Install Homebrew Packages
+brew brewdle
 
 # Install NeoBundle
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh

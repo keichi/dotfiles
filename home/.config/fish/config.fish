1b69ca8b903846ceac99368b9e63aef18ac459ca
Keichi Takahashi <keichi.t@me.com>
2016-03-15 14:41:23 +0900

    El Capitanにより/usr以下が書き込み禁止になったのに対応

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -16,7 +16,7 @@ Plugin "rbenv"
 Plugin "bundler"
 
 set -x PATH $HOME/Library/Haskell/bin $PATH
-set -x PATH /usr/texbin $PATH
+set -x PATH /usr/local/texlive/2013/bin/x86_64-darwin $PATH
 set -x PATH /usr/local/sbin $PATH
 set -x PATH /usr/local/gcc-arm/bin $PATH
 

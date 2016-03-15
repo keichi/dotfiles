792122339b85054911c154082a13573f4c7fca15
Keichi Takahashi <keichi.t@me.com>
2016-03-15 14:41:03 +0900

    OSX内蔵のopensshがECDSAに対応したので削除

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -50,45 +50,6 @@ function fish_user_key_bindings
     fish_vi_key_bindings
 end
 
-# ssh-agent
-setenv SSH_ENV $HOME/.ssh/environment
-
-function start_agent
-    echo "Initializing new SSH agent ..."
-    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-    echo "succeeded"
-    chmod 600 $SSH_ENV
-    . $SSH_ENV > /dev/null
-    ssh-add
-end
-
-function test_identities
-    ssh-add -l | grep "The agent has no identities" > /dev/null
-    if [ $status -eq 0 ]
-        ssh-add
-        if [ $status -eq 2 ]
-            start_agent
-        end
-    end
-end
-
-if [ -n "$SSH_AGENT_PID" ]
-    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    end
-else
-    if [ -f $SSH_ENV ]
-        . $SSH_ENV > /dev/null
-    end
-    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
-    if [ $status -eq 0 ]
-        test_identities
-    else
-        start_agent
-    end
-end
-
 function fish_prompt
     ~/powerline-shell.py $status --shell bare ^/dev/null
 end

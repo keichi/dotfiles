32e1268b2ba102cfa31b912c506d8ac7cb0c255d
Keichi Takahashi <keichi.t@me.com>
2016-04-03 22:39:09 +0900

    oh-my-fishをアップデート

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,19 +1,8 @@
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+# Path to Oh My Fish install.
+set -gx OMF_PATH "/Users/keichi/.local/share/omf"
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
-
-# Theme
-Theme "robbyrussell"
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-Plugin "brew"
-Plugin "node"
-Plugin "rbenv"
-Plugin "bundler"
+source $OMF_PATH/init.fish
 
 set -x PATH $HOME/Library/Haskell/bin $PATH
 set -x PATH /usr/local/texlive/2013/bin/x86_64-darwin $PATH
@@ -23,9 +12,6 @@ set -x PATH /usr/local/gcc-arm/bin $PATH
 set -x EDITOR vim
 
 set -x LC_ALL ja_JP.UTF-8
-
-# Python
-eval (python -m virtualfish auto_activation)
 
 # Go
 set -x GOPATH $HOME/go
@@ -42,13 +28,6 @@ alias cp="rsync --archive --human-readable --progress --verbose --whole-file"
 # to watch."
 # c.f. https://github.com/aspnet/Home/issues/508
 set -x MONO_MANAGED_WATCHER false
-
-# direnv
-eval (direnv hook fish)
-
-function fish_user_key_bindings
-    fish_vi_key_bindings
-end
 
 function fish_prompt
     ~/powerline-shell.py $status --shell bare ^/dev/null

8e8ef37041c11b6b3714d10551018798f10207af
Keichi Takahashi <keichi.t@me.com>
2016-04-25 11:36:48 +0900

    Update initial bootstrap script

--- home/.install.sh
+++ home/.install.sh
@@ -2,13 +2,8 @@
 
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap Homebrew/brewdler
-
-# Install Homebrew Packages
-brew brewdle
-
-# Install NeoBundle
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+brew install rcmdnk/file/brew-file
+brew file install
 
 # Install oh-my-fish
 curl -L https://github.com/bpinto/oh-my-fish/raw/master/tools/install.fish | fish

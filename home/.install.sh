822a6335d452bbb257c35b6a33f6e12c776cdb6e
Keichi Takahashi <keichi.t@me.com>
2016-04-25 11:53:34 +0900

    初期セットアップ時にhomeshickでcastleを入れ忘れていた

--- home/.install.sh
+++ home/.install.sh
@@ -2,6 +2,8 @@
 
 # Install homeshick
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
+source "$HOME/.homesick/repos/homeshick/homeshick.sh"
+homeshick clone git://github.com/keichi/dotfiles.git
 
 # Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
